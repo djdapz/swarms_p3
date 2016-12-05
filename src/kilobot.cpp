c7f09cf3abb87065947603b4aff3d4449dbd6d06
djdapz <djdapz@aol.com>
2016-12-04 20:28:13 -0600

    logging

--- src/kilobot.cpp
+++ src/kilobot.cpp
@@ -187,6 +187,9 @@ class mykilobot : public kilobot
 		if(id == 1){
 			set_color(RGB(1,0,0));
 			std::cout<<"here"<<std::endl;
+			std::cout<<"force_mag: "<<force_mag<<std::endl;
+			std::cout<<"ticks: "<<ticks<<std::endl;
+
 		}
 
 
