b05d7a3df83d5596cbdabf082f27f31770c60a90
djdapz <djdapz@aol.com>
2016-12-08 19:19:11 -0600

    other

--- src/kilobot.cpp
+++ src/kilobot.cpp
@@ -83,14 +83,15 @@ class mykilobot : public kilobot
 			movement_mag = sqrt(running_x*running_x + running_y*running_y);
 			if(id ==1){
 				printf("=====================\n\r");
-				printf("running_x	   %f\n\r", running_x);
-				printf("running_y	   %f\n\r", running_y);
-				printf("movement_mag:  %f\n\r", movement_mag);
-				printf("compass:       %d\n\r", compass_deg);
-				printf("next_angle:    %d\n\r", next_angle_deg);
-				printf("ticks:         %d\n\r", ticks);
-				printf("gravity:       %d\n\r", gravity);
-				printf("distance:      %d\n\r", distance);
+				printf("running_x	          %f\n\r", running_x);
+				printf("running_y	          %f\n\r", running_y);
+				printf("composite_dir_rad	  %f\n\r", running_y);
+				printf("movement_mag: 	      %f\n\r", movement_mag);
+				printf("compass:     	      %d\n\r", compass_deg);
+				printf("next_angle:  	      %d\n\r", next_angle_deg);
+				printf("ticks:         	      %d\n\r", ticks);
+				printf("gravity:              %d\n\r", gravity);
+				printf("distance:    	      %d\n\r", distance);
 			}
 
 			running_x = 0;
